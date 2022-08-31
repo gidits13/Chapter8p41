773719dc3f8707f145293be154215d063ddd1bb0
gidits13 <gidits13@gmail.com>
2022-08-31 12:14:57 +0300

    Добавьте файлы проекта.

---
+++ Chapter8p41/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+namespace Chapter8p41
+{
+    
+    internal class Program
+    {
+        const string FileName = @"C:\2\BinaryFile.bin";
+        static void WriteFile(string fileName)
+        {
+            if (File.Exists(FileName))
+            {
+                using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(FileName)))
+                {
+                    bw.Write($"файл изменен:{DateTime.Now} на ОС {Environment.OSVersion}" );
+                }
+            }
+        }
+        static void ReadFile()
+        {
+            if (File.Exists(FileName))
+            {
+                string date;
+                string str;
+                using(BinaryReader br = new BinaryReader(File.OpenRead(FileName)))
+                {
+                    date = br.ReadString();
+                //    str = br.ReadString();
+                }
+                Console.WriteLine(date);
+               // Console.WriteLine(str);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            WriteFile(FileName);
+            ReadFile();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
